0b38d81804f2860b02124e60a8feca6351f98f37
Meshbola15 <Meshbola15@gmail.com>
2025-12-15 10:35:51 +0100

    chore: Add production host to allowedHosts in Vite server configuration for deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   server: {
     port: 5173,
     host: true,
-    allowedHosts: ['all'],
+    allowedHosts: ['all', "bitcoinvillagexfrontend-production.up.railway.app"],
   },
   build: {
     outDir: 'dist',
